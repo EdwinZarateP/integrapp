24966af27d95e565d902d16012e6deb507553a14
EdwinZarateP <emzp1994@gmail.com>
2025-01-31 19:23:32 -0500

    se corrige

--- src/Paginas/SeleccionEstados/index.tsx
+++ src/Paginas/SeleccionEstados/index.tsx
@@ -6,37 +6,28 @@ import { FaTruck } from "react-icons/fa6";
 import { IoDocumentsSharp } from "react-icons/io5";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import Cookies from 'js-cookie';
-import { useNavigate } from "react-router-dom";
 import logo from "../../Imagenes/albatros.png";
 import { ContextoApp } from "../../Contexto/index";
 
 const EstadosManifiestos = () => {
   const navigate = useNavigate();
   const almacenVariables = useContext(ContextoApp);
-  const navigate = useNavigate();
 
   const handleNavigation = (estado: string) => {
     almacenVariables?.setEstado(estado);
     navigate('/Estados');
   };
 
-
   // Función para manejar el cierre de sesión
   const cerrarSesion = () => {
-    // Remover las cookies
     Cookies.remove("nombreIntegrapp");
-    // Redirigir al inicio y recargar la página
     navigate("/");
   };
 
   return (
     <div className="contenedorManifiestos-principal">
       <header className="contenedorManifiestos-cabecera">
-        <img
-          src={logo}
-          alt="Logo Integra"
-          className="contenedorManifiestos-logo"
-        />
+        <img src={logo} alt="Logo Integra" className="contenedorManifiestos-logo" />
         <div className="contenedorManifiestos-titulo">
           <h1>Integr</h1>
           <h1>App</h1>
@@ -44,35 +35,18 @@ const EstadosManifiestos = () => {
       </header>
 
       <div className="contenedorManifiestos-estados">
-        <div 
-          onClick={() => handleNavigation("TRANSITO")}
-          className="contenedorManifiestos-linkBoton"
-        >
+        <div onClick={() => handleNavigation("TRANSITO")} className="contenedorManifiestos-linkBoton">
           <BotonEstado nombreEstado="En tránsito" icono={<FaTruck />} />
         </div>
 
-        <div 
-          onClick={() => handleNavigation("CUMPLIDO")}
-          className="contenedorManifiestos-linkBoton"
-        >
+        <div onClick={() => handleNavigation("CUMPLIDO")} className="contenedorManifiestos-linkBoton">
           <BotonEstado nombreEstado="Cumplidos" icono={<IoDocumentsSharp />} />
         </div>
 
-        <div 
-          onClick={() => handleNavigation("LIQUIDADO")}
-          className="contenedorManifiestos-linkBoton"
-        >
+        <div onClick={() => handleNavigation("LIQUIDADO")} className="contenedorManifiestos-linkBoton">
           <BotonEstado nombreEstado="Liquidados" icono={<FaMoneyBillTransfer />} />
         </div>
-        </Link>
 
-        {/* Botón de cierre de sesión */}
-        <BotonSencillo
-          type="button"
-          texto="Salir"
-          colorClass="rojo"
-          onClick={cerrarSesion} // Asignar la función aquí
-        />
       </div>
     </div>
   );
